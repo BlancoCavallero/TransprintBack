f6b50932743e5c832fd31b02090808838621f5ad
maxitiero <tieromaximiliano@gmail.com>
2025-09-17 20:12:48 -0300

    chore(init.sql): replace init.sql with new database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const authRoutes = require('./src/routes/auth');
 const userRoutes = require('./src/routes/users');
+//const clientRoutes = require('./src/routes/clients');
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,8 @@ app.use(express.json());
 
 app.use('/api', authRoutes); 
 app.use('/api/users', userRoutes);
+//app.use('/api/clients', clientRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
