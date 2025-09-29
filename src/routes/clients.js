d8f5793870035727d0212266d78444a1cecf2dfc
maxitiero <tieromaximiliano@gmail.com>
2025-09-29 15:14:19 -0300

    feat (driverController.js):add CRUD, truck assignment, availability check, and history check

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -10,6 +10,6 @@ router.get("/:id", clientController.obtenerClientePorId); // Buscar por ID
 router.post("/", validarCliente, validarResultado, clientController.registrarCliente); // Crear
 router.put("/:id", validarCliente, validarResultado, clientController.actualizarCliente); // Actualizar
 router.delete("/:id", clientController.eliminarCliente); // Eliminar
-           
+       
 
 module.exports = router;
