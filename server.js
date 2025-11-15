0ec86dd1cf5cb5d97699c8cb91e1b7af95d1c952
tirs00 <tirso.biardo@gmail.com>
2025-11-15 20:58:20 -0300

    CRUD de Mantenimiento hecho

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ app.use('/api/users', userRoutes);
 app.use('/api/clients', clientRoutes);
 app.use('/api/persons', personaRoutes);
 
+const vehiculosRoutes = require("./src/routes/vehiculos");
+app.use("/api/vehiculos", vehiculosRoutes);
+
+const mantenimientosRoutes = require("./src/routes/mantenimientos");
+app.use("/api/mantenimientos", mantenimientosRoutes);
+
 // Middleware global de errores
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const vehiculosRoutes = require("./src/routes/vehiculos");
-app.use("/api/vehiculos", vehiculosRoutes);
