2ca59e957346676c98deadb920973c8ae059a55c
maxitiero <tieromaximiliano@gmail.com>
2026-02-10 21:35:20 -0300

    chore(dashboardController): elimino console.log

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -4,7 +4,7 @@ const { successResponse, errorResponse } = require('../utils/response');
 
 const obtenerDashboard = async (req, res, next) => {
     try {
-        console.log('📊 [Dashboard] Solicitando datos...');
+        //console.log('📊 [Dashboard] Solicitando datos...');
         
         // 1. Llamamos al SERVICE para que haga los cálculos
         const datosDashboard = await dashboardService.obtenerDatosDashboard();
@@ -12,7 +12,7 @@ const obtenerDashboard = async (req, res, next) => {
         // 2. Enviamos la respuesta CON ÉXITO
         successResponse(res, datosDashboard, "Dashboard obtenido exitosamente");
         
-        console.log('✅ [Dashboard] Datos enviados correctamente');
+        //console.log('✅ [Dashboard] Datos enviados correctamente');
     } catch (error) {
         // 3. Si algo sale MAL, manejamos el error
         console.error('❌ [Dashboard] Error:', error.message);
